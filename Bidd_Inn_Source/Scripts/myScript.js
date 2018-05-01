fdad88563c9eba3f0ccb411bce1f14d429d11b72
Joseph-dias <38766207+Joseph-dias@users.noreply.github.com>
2018-04-30 19:02:50 -0700

    Change to javascript

--- Bidd_Inn_Source/Scripts/myScript.js
+++ Bidd_Inn_Source/Scripts/myScript.js
@@ -1,8 +1,8 @@
-﻿var i = setInterval(changeNums, 1000);
+﻿window.setInterval(changeNums, 1000);
 var custs = "<%=customerNumber%>";
 var hots = "<%=hotelNumber%>";
 
 function changeNums() {
-    document.getElementById("cNum").innerText = custs;
-    document.getElementById("hNum").innerText = hots;
+    document.getElementById("#cNum").innerText = custs;
+    document.getElementById("#hNum").innerText = hots;
 }
